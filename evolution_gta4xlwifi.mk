84bb758f5dc43640b7d7adf286dd2aa28c44bf95
Chuqol <151944573+chuqol@users.noreply.github.com>
2024-02-28 08:43:58 +0100

    Add files via upload

---
+++ evolution_gta4xlwifi.mk
@@ -0,0 +1,44 @@
+#
+# Copyright (C) 2020-2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+## Inherit from generic products, most specific first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
+## Inherit from gta4xlwifi device
+$(call inherit-product, device/samsung/gta4xlwifi/device.mk)
+
+# EvolutionX Stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_TOUCHGESTURES := true 
+
+## Inherit some common EvolutionX stuff
+$(call inherit-product, vendor/evolution/config/common_full_tablet_wifionly.mk)
+
+## Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+## Device identifier, this must come after all inclusions
+PRODUCT_DEVICE := gta4xlwifi
+PRODUCT_NAME := evolution_gta4xlwifi
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-P610
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
